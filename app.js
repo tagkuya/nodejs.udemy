d0785449e677eb789ade5279223bb0c079496341
tagkuya <fortis931taku@gmail.com>
2018-07-07 21:06:58 +0900

    systemlog実装

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+var systemlogger = require("./lib/log/systemlogger.js");
 var express = require("express");
 var app = express();
 
 app.set("view engine","ejs");
+app.disabled("x-powerd-by");
 
 app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use("/",require("./routes/index.js"));
+
+app.use(systemlogger());
 
 app.listen(3000);
---
+++ config/log4js.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+var ROOT = path.join(__dirname, "../");
+
+module.exports = {
+  appenders:{
+    ConsoleLogAppender:{
+      type:"console"
+    },
+    FileLogAppender:{
+      type: "file",
+      filename: path.join(ROOT, "./log/system.system.log"),
+      maxLogSize : 5000000,
+      backups: 10
+    }
+  },
+  categories:{
+    "default":{
+      appenders:["ConsoleLogAppender"],
+      level:"ALL"
+    },
+    system: {
+      appenders : ["FileLogAppender"],
+      level : "ERROR"
+    }
+  }
+};
