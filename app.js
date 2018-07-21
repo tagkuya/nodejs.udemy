a47a68cc9a43e999dbd0e2ebfd2b1e45b0b18319
takuya shimobayashi <fortis931taku@gmail.com>
2018-07-21 21:31:58 +0900

    記事投稿実装

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 var accesslogger = require("./lib/log/accesslogger.js");
 var systemlogger = require("./lib/log/systemlogger.js");
+var bodyParser = require("body-parser");
 var express = require("express");
 var app = express();
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.disabled("x-powerd-by");
 
 app.use("/public", express.static(__dirname + "/public/" + (process.env.NODE_ENV === "development" ? "development" : "production")));
 
 app.use(accesslogger());
 
-app.use("/",require("./routes/index.js"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/", require("./routes/index.js"));
 app.use("/posts", require("./routes/posts.js"));
 app.use("/search", require("./routes/search"));
 app.use("/account", require("./routes/account"));
--- routes/account.js
+++ routes/account.js
@@ -1,7 +1,29 @@
 var router = require("express").Router();
 
-router.get("/",(req,res)=>{
+var createRegistData = function (body) {
+  var datetime = new Date();
+  return {
+    url: bodr.url,
+    published: datetime,
+    update: datetime,
+    title: body.title,
+    content: body.content,
+    keywords: (body.keywords || "").split(''),
+    authors: (body.authors || "").split('')
+  };
+};
+
+router.get("/", (req, res) => {
   res.render("./account/index.ejs");
+});
+
+router.get("/posts/regist", (req, res) => {
+  res.render("./account/posts/regist-form.ejs");
+});
+
+router.post("/posts/regist/input", (req, res) => {
+  var original = createRegistData(req.body);
+  res.render("./account/posts/regist-form.ejs", { original });
 });
 
 module.exports = router;
